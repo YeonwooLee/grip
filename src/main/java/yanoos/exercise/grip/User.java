6b8d06f12932298a43f0814370a97f5c96980821
YeonwooLee <fox_93@naver.com>
2022-11-26 17:54:10 +0900

    임시 유저 템플릿 생성

---
+++ src/main/java/yanoos/exercise/grip/User.java
@@ -0,0 +1,12 @@
+package yanoos.exercise.grip;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class User {
+    int id;
+    String email;
+    String nickname;
+}
