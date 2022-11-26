18bd2383c376340904739b78609030e4807aa2d3
YeonwooLee <fox_93@naver.com>
2022-11-26 18:05:43 +0900

    configuration 생성

---
+++ src/main/java/yanoos/exercise/grip/MainConfiguration.java
@@ -0,0 +1,10 @@
+package yanoos.exercise.grip;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@ComponentScan
+//역할 까먹음 ㅠ
+public class MainConfiguration implements WebMvcConfigurer  {
+}
