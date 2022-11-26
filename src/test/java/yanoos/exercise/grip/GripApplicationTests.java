674b368a464c4fe4a204a0407de61eb67621cf49
YeonwooLee <fox_93@naver.com>
2022-11-26 17:32:13 +0900

    프로젝트 생성

---
+++ src/test/java/yanoos/exercise/grip/GripApplicationTests.java
@@ -0,0 +1,13 @@
+package yanoos.exercise.grip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
